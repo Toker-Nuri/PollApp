26d52567295112bb37adc940ec92619fd4e24712
Nuri Toker <n.toker@yahoo.com>
2026-05-15 08:52:38 +0200

    first cimmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CounterService } from './counter_service';
+import { CompA } from './comp-a/comp-a.component';
+import { CompB } from './comp-b/comp-b.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CompA, CompB],
+  providers: [CounterService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'PollApp';
+  counterService = inject(CounterService);
 }
---
+++ src/app/counter_service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CounterService {
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+}
